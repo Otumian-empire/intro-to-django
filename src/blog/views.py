9a79fdd3614fbf2ed7854a0dcdd43c2663fc4ea4
otumian <popecan1000@gmail.com>
2019-12-25 18:28:13 +0000

    use slug for unique url generation

--- src/blog/views.py
+++ src/blog/views.py
@@ -11,7 +11,17 @@ from .models import BlogPost
 #     context = BlogPost.objects.get(id=1)
 #     return render(request, 'blog_post_details.html', {'context': context})
 
-# making the view dynamic or building dynamic urls
-def blog_post_details_page(request, post_id):
-    context = get_object_or_404(BlogPost, id=post_id)
+# # making the view dynamic or building dynamic urls
+# def blog_post_details_page(request, post_id):
+#     context = get_object_or_404(BlogPost, id=post_id)
+#     return render(request, 'blog_post_details.html', {'context': context})
+
+# making the view dynamic or building dynamic urls using slugs instead of ids
+def blog_post_details_page(request, slug):
+    # queryset = BlogPost.objects.filter(slug=slug)
+    # if queryset.count() == 0:
+    #     raise Http404
+
+    # context = queryset.first()
+    context = get_object_or_404(BlogPost, slug=slug)
     return render(request, 'blog_post_details.html', {'context': context})
